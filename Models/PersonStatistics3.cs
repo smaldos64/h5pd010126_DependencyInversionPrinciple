465da45bbd608fc1dd81ea1e4c427897a32c336a
smaldos64 <gudumholm34@gmail.com>
2026-01-23 13:48:50 +0100

    Lavet ændringer/tilføjelser rundt omkring efter fremlægelse på Projktor.

--- Models/PersonStatistics3.cs
+++ Models/PersonStatistics3.cs
@@ -8,19 +8,6 @@ using System.Threading.Tasks;
 
 namespace DependencyInversionPrinciple.Models
 {
-    //public class PersonStatistics3<T> where T : class, IPersonData3, IEmployeeSearchable3<T>
-    //{
-    //    private readonly T _emp;
-
-    //    public PersonStatistics3(T emp)
-    //    {
-    //        _emp = emp;
-    //    }
-
-    //    public int CountFemaleManagers() =>
-    //    _emp.GetDataByGenderAndPosition(Gender.Female, Position.Manager).Count();
-    //}
-
     public class PersonStatistics3<T> where T : class, IPersonData3
     {
         // Vi gemmer en reference til interfacet, ikke til T
--- Program.cs
+++ Program.cs
@@ -30,8 +30,24 @@ namespace DependencyInversionPrinciple
             empManager3.AddPerson(new Student3 { Name = "Mike_3", Gender = Gender.Male, Position = Position.Administrator, Team = Team.h5xpd011126 });
             empManager3.AddPerson(new Student3 { Name = "Helle_3", Gender = Gender.Female, Position = Position.Manager, Team = Team.h6pd040126 });
             empManager3.AddPerson(new Student3 { Name = "Hanne_3", Gender = Gender.Female, Position = Position.Manager, Team = Team.h6pd040126 });
+            // Linjen herunder kan IKKE compilere
+            //empManager3.AddPerson(new Employee3 { Name = "Test_3", Gender = Gender.Female, Position = Position.Manager});
             var stats3 = new PersonStatistics3<Student3>(empManager3);
-            Console.WriteLine($"Number of female managers among Employees (3) is : {stats3.CountFemaleManagers()}");
+            Console.WriteLine($"Number of female managers among Students (3) is : {stats3.CountFemaleManagers()}");
+            
+            EmployeeManager3<Employee3> empManager3_1 = new EmployeeManager3<Employee3>();
+            empManager3_1.AddPerson(new Employee3 { Name = "Test_3", Gender = Gender.Female, Position = Position.Manager });
+            empManager3_1.AddPerson(new Employee3 { Name = "Test_3_1", Gender = Gender.Female, Position = Position.Manager });
+            // Linjen herunder kan IKKE compilere
+            //empManager3_1.AddPerson(new Student3 { Name = "Mette_3", Gender = Gender.Female, Position = Position.Manager, Team = Team.h5pd010126 });
+            var stats3_1 = new PersonStatistics3<Employee3>(empManager3_1);
+            Console.WriteLine($"Number of female managers among Employees (3_1) is : {stats3_1.CountFemaleManagers()}");
+            
+            EmployeeManager3<IPersonData3> empManager3a = new EmployeeManager3<IPersonData3>();
+            empManager3a.AddPerson(new Student3 { Name = "Mette_3", Gender = Gender.Female, Position = Position.Manager, Team = Team.h5pd010126 });
+            empManager3a.AddPerson(new Employee3 { Name = "Test_3", Gender = Gender.Female, Position = Position.Manager });
+            var stats3_a = new PersonStatistics3<IPersonData3>(empManager3a);
+            Console.WriteLine($"Number of female managers among Persons (Employees - Students) (3_2) is : {stats3_a.CountFemaleManagers()}");
             Console.WriteLine();
 
             EmployeeManager4<Student4> empManager4 = new EmployeeManager4<Student4>();
@@ -52,17 +68,25 @@ namespace DependencyInversionPrinciple
             empManager5.AddPerson(new Employee5 { Name = "Hans_5", Gender = Gender.Male, Position = Position.Manager });
             var stats5 = new PersonStatistics5<IPersonData5>(empManager5);
             femaleAndManagersCount = stats5.CountData(s => s.Gender == Gender.Female && s.Position == Position.Manager);
-            Console.WriteLine($"Number of female and managers among Employees/Students (5) is : {femaleAndManagersCount}");
+            Console.WriteLine($"Number of female and managers among Persons (Employees - Students) (5) is : {femaleAndManagersCount}");
+            
             femaleOrManagersCount = stats5.CountData(s => s.Gender == Gender.Female || s.Position == Position.Manager);
-            Console.WriteLine($"Number of female or managers among Employees/Students (5_1) is : {femaleOrManagersCount}");
+            Console.WriteLine($"Number of female or managers among Persons (Employees - Students) (5_1) is : {femaleOrManagersCount}");
+            
             int femaleAndManagersCountInStudents = stats5.CountData(s => s is Student5 && s.Position == Position.Manager && s.Gender == Gender.Female);
             Console.WriteLine($"Number of female managers among Students (5_2) is : {femaleAndManagersCountInStudents}");
+            
             int maleAndManagersCountInStudents = stats5.CountData(s => s is Student5 && s.Position == Position.Manager && s.Gender == Gender.Male);
             Console.WriteLine($"Number of male managers among Students (5_3) is : {maleAndManagersCountInStudents}");
+            
             int numberOfStudentsInList = stats5.CountType<Student5>();
             Console.WriteLine($"Number of Student Objects in List (5_4) is : {numberOfStudentsInList}");
+            
             int numberOfEmployeesInList = stats5.CountType<Employee5>();
             Console.WriteLine($"Number of Employees Objects in List (5_5) is : {numberOfEmployeesInList}");
+            
+            int numberOfPersonsInList = stats5.CountType<IPersonData5>();
+            Console.WriteLine($"Number of Persons (Employees - Students) Objects in List (5_6) is : {numberOfPersonsInList}");
             Console.WriteLine();
 
             List<IPersonData5> iPersondataList = stats5.GetAllData();
